4ac07690a8571516687e12046a42101a58c32ce6
shimizu-saffle <shimizu.saffle@gmail.com>
2022-05-17 18:17:04 +0900

    ✨feat: アプリ起動時に匿名認証でログイン & ログアウト機能を実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shopping_list_sample/controllers/auth_controller.dart';
 
 import 'firebase_options.dart';
 
@@ -8,7 +10,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,16 +19,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyWidget(),
+      home: MyScreen(),
     );
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class MyScreen extends HookConsumerWidget {
+  const MyScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authController = ref.watch(authControllerProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+        leading: authController != null
+            ? IconButton(
+                onPressed: () =>
+                    ref.read(authControllerProvider.notifier).signOut(),
+                icon: const Icon(Icons.logout),
+              )
+            : null,
+      ),
+    );
   }
 }
