e5226f7d3779fe42ae99cdeba41032b469b57a72
shimizu-saffle <shimizu.saffle@gmail.com>
2022-05-17 17:24:32 +0900

    define: AuthRepository

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shopping_list_sample/general_providers.dart';
+
+import 'custom_exception.dart';
+
+abstract class BaseAuthRepository {
+  Stream<User?> get authStateChanges;
+  Future<void> signInAnonymously();
+  User? getCurrentUser();
+  Future<void> signOut();
+}
+
+final authRepositoryProvider =
+    Provider<AuthRepository>((ref) => AuthRepository(ref.read));
+
+class AuthRepository implements BaseAuthRepository {
+  const AuthRepository(this._read);
+
+  final Reader _read;
+
+  @override
+  Stream<User?> get authStateChanges =>
+      _read(firebaseAuthProvider).authStateChanges();
+
+  @override
+  User? getCurrentUser() {
+    try {
+      return _read(firebaseAuthProvider).currentUser;
+    } on FirebaseAuthException catch (e) {
+      throw CustomException(message: e.message);
+    }
+  }
+
+  @override
+  Future<void> signInAnonymously() async =>
+      await _read(firebaseAuthProvider).signInAnonymously();
+
+  @override
+  Future<void> signOut() async {
+    try {
+      await _read(firebaseAuthProvider).signOut();
+      await signInAnonymously();
+    } on FirebaseAuthException catch (e) {
+      throw CustomException(message: e.message);
+    }
+  }
+}
