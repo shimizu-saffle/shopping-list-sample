0bb599c34631800d3f7f698069f9acacf919596e
shimizu-saffle <shimizu.saffle@gmail.com>
2022-05-18 11:35:43 +0900

    define: ItemRepository

---
+++ lib/repositories/item_repository.dart
@@ -0,0 +1,80 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shopping_list_sample/extensions/firebase_firestore_extension.dart';
+
+import '../general_providers.dart';
+import '../models/item_model.dart';
+import 'custom_exception.dart';
+
+abstract class BaseItemRepository {
+  Future<List<Item>> retrieveItems({required String userId});
+  Future<String> createItem({required String userId, required Item item});
+  Future<void> updateItem({required String userId, required Item item});
+  Future<void> deleteItem({required String userId, required String itemId});
+}
+
+class ItemRepository implements BaseItemRepository {
+  ItemRepository(this._read);
+
+  final Reader _read;
+
+  // TODO(shimizu-saffle): itemConverterを定義する
+
+  @override
+  Future<String> createItem({
+    required String userId,
+    required Item item,
+  }) async {
+    try {
+      final docRef = await _read(firebaseFirestoreProvider)
+          .usersListRef(userId)
+          .add(item.toDocument());
+      return docRef.id;
+    } on FirebaseException catch (e) {
+      throw CustomException(message: e.message);
+    }
+  }
+
+  @override
+  Future<void> deleteItem({
+    required String userId,
+    required String itemId,
+  }) async {
+    try {
+      await _read(firebaseFirestoreProvider)
+          .usersListRef(userId)
+          .doc(itemId)
+          .delete();
+    } on FirebaseException catch (e) {
+      throw CustomException(message: e.message);
+    }
+  }
+
+  @override
+  Future<List<Item>> retrieveItems({required String userId}) async {
+    try {
+      final snap =
+          await _read(firebaseFirestoreProvider).usersListRef(userId).get();
+      return snap.docs
+          .map(
+            (doc) => Item.fromDocument(
+                doc as QueryDocumentSnapshot<Map<String, dynamic>>),
+          )
+          .toList();
+    } on FirebaseException catch (e) {
+      throw CustomException(message: e.message);
+    }
+  }
+
+  @override
+  Future<void> updateItem({required String userId, required Item item}) async {
+    try {
+      await _read(firebaseFirestoreProvider)
+          .usersListRef(userId)
+          .doc(item.id)
+          .update(item.toDocument());
+    } on FirebaseException catch (e) {
+      throw CustomException(message: e.message);
+    }
+  }
+}
